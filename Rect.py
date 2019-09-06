0072f110cd83bf0b273fa776c355f63382079b11
Cesar Ivan Delgado Medina <cdelgado@informacolombia.com>
2019-09-06 18:39:12 -0500

    Cambio de movimiento del personaje de mouse a teclado (se aplican algunas mejoras)

--- Rect.py
+++ Rect.py
@@ -8,48 +8,52 @@ class Rect():
         self.width = _width
         self.height = _height
         self.color = _color
+        self.friction = 0.98
         self.vectPos = pygame.Vector2(0, 0)
-        self.normal = pygame.Vector2(0, 0)
-        self.penetration = 1
-        self.min = pygame.Vector2(self.vectPos.x, self.vectPos.y + self.height)
-        self.max = pygame.Vector2(self.vectPos.x + self.width, self.vectPos.y )
+        self.velocity = pygame.Vector2(0, 0)
         self.rect = pygame.Rect((self.vectPos.x, self.vectPos.y), (self.width, self.height))
-        self.in_floor = False
     
-    def __update_rect(self):
-        self.min = pygame.Vector2(self.vectPos.x, self.vectPos.y + self.height)
-        self.max = pygame.Vector2(self.vectPos.x + self.width, self.vectPos.y )
+    def update(self, dt):
+        self.accelerate(dt)
+        self.detect_limit_window()
         self.rect = pygame.Rect((self.vectPos.x, self.vectPos.y), (self.width, self.height))
 
     def detect_limit_window(self):
         if (self.vectPos.x < 0):
             self.vectPos.x = 0
-        elif (self.vectPos.x + self.width > self.size_win[0]):
+            self.velocity.x *= -1
+        if (self.vectPos.x + self.width > self.size_win[0]):
             self.vectPos.x = self.size_win[0] - self.width
-        elif (self.vectPos.y < 0):
+            self.velocity.x *= -1
+        if (self.vectPos.y < 0):
             self.vectPos.y = 0
-        elif (self.vectPos.y + self.height > self.size_win[1]):
+            self.velocity.y *= -1
+        if (self.vectPos.y + self.height > self.size_win[1]):
             self.vectPos.y = self.size_win[1] - self.height
-            self.in_floor = True
-        self.__update_rect()
+            self.velocity.y *= -1
 
     def draw(self):
         pygame.draw.rect(self.window, self.color, self.rect)
 
-    def accelerate(self, velocity):
-        self.vectPos += velocity
-        self.detect_limit_window()
+    def accelerate(self, dt):
+        self.vectPos += self.velocity * dt
 
+    def desacelerar(self, friction):
+        if (self.velocity.x > 0):
+            self.velocity.x -= friction
+        else:
+            self.velocity.x += friction
+        if (self.velocity.y > 0):
+            self.velocity.y -= friction
+        else:
+            self.velocity.y += friction
+        
     def set_position(self, _posX, _posY):
         self.vectPos.x = _posX
         self.vectPos.y = _posY
-        self.detect_limit_window()
 
     def set_color(self, _color):
         self.color = _color
-
-    def get_AABB(self):
-        return 
 
     def get_rect(self):
         return self.rect
--- main.py
+++ main.py
@@ -8,11 +8,10 @@ class ColisionesRect():
     def __init__(self):
         self.clock = pygame.time.Clock()
         self.start_game()
-        self.gravity = pygame.Vector2(0, 9.86)
         self.WHITE = 255, 255, 255
         self.BLACK = 0, 0, 0
         self.RED = 255, 0, 0
-        self.wind_size = (500, 500)
+        self.wind_size = (600, 600)
         self.WIN_WIDTH = self.wind_size[0]
         self.WIN_HEIGHT = self.wind_size[1]
         self.window = pygame.display.set_mode(self.wind_size, 0, 0, 0)
@@ -23,8 +22,9 @@ class ColisionesRect():
     
     def start_game(self):
         self.game_over = False
-        self.mouse_in = False
-        self.velocity = pygame.Vector2(0, 0)
+        self.vel = 5
+        self.colision_x = False
+        self.colision_y = False
     
     def loop(self):
         self.dt = 0
@@ -36,21 +36,9 @@ class ColisionesRect():
             self.dt = self.clock.tick(60) / 1000.0
             
     def update(self, dt):
-        if (self.mouse_in):
-            coords_mouse = pygame.mouse.get_pos()
-            centerW = self.rect.width/2
-            centerH = self.rect.height/2
-            self.rect.set_position(coords_mouse[0] - centerW, coords_mouse[1] - centerH)
-            self.rect.in_floor = False
-        else:
-             self.velocity += self.gravity * dt
-             self.rect.accelerate(self.velocity)
-        if (self.rect.in_floor):
-            self.velocity.y = 0
-            self.activar_gravity(False)
-        else:
-            self.activar_gravity(True)
-        self.collision_AABB(self.rect, self.rect_static)
+        self.rect.update(dt) 
+        self.rect_static.update(dt)
+        self.detect_colision(self.rect, self.rect_static)
     
     def render(self):
         self.window.fill(self.BLACK)
@@ -62,35 +50,40 @@ class ColisionesRect():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        mouse = pygame.mouse.get_pressed()
-        if (mouse[0]):
-            coords_mouse = pygame.mouse.get_pos()
-            if (self.rect.get_rect().collidepoint(coords_mouse)):
-                self.mouse_in = True
-        else:
-            self.mouse_in = False
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_UP]):
+            self.rect.velocity.y -= self.vel
+        if (keys[pygame.K_DOWN]):
+            self.rect.velocity.y += self.vel
+        if (keys[pygame.K_RIGHT]):
+            self.rect.velocity.x += self.vel
+        if (keys[pygame.K_LEFT]):
+            self.rect.velocity.x -= self.vel
+        self.rect.desacelerar(self.rect.friction)
 
-    def collision_AABB(self, rect1, rect2):
-        if (rect1.get_rect().colliderect(rect2.get_rect())):
+    def detect_colision(self, rect1, rect2):
+        if (self.AABB_vs_AABB(rect1, rect2)):
             rect2.set_color(self.RED)
-            #detectar y corregir la colision entre los rectangulos
-            self.AABB_vs_AABB(rect1, rect2)
+            self.resolve_collision(rect1, rect2)
         else:
             rect2.set_color(self.WHITE)
 
     def AABB_vs_AABB(self, rect1, rect2):
-        if (rect1.vectPos.y + rect1.height > rect2.vectPos.y and rect1.vectPos.y > 0):
-            rect1.vectPos.y = rect2.vectPos.y - rect1.height
-            self.rect.in_floor = True
-        elif (rect1.vectPos.x < rect2.vectPos.x + rect2.width and rect1.vectPos.x < 0):
-            rect1.vectPos.x = rect2.vectPos.x + rect2.width
-            
-
-    def activar_gravity(self, active):
-        if (active):
-            self.gravity.y = 9.86
+        if (rect1.vectPos.x + rect1.width >= rect2.vectPos.x and
+            rect2.vectPos.x + rect2.width >= rect1.vectPos.x):
+            self.colision_x = True
         else:
-            self.gravity.y = 0
+            self.colision_x = False
+        if (rect1.vectPos.y + rect1.height >= rect2.vectPos.y and
+            rect2.vectPos.y + rect2.height >= rect1.vectPos.y):
+            self.colision_y = True
+        else:
+            self.colision_x = False
+        return self.colision_x and self.colision_y
+    
+    def resolve_collision(self, rect1, rect2):
+        pass
+        
 
 if __name__ == "__main__":
     play = ColisionesRect()
